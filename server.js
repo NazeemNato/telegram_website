6eb7df5377b3ec0e2d6503523879c8a8de0cc3a0
nazeemnato <nazeemnob18@gmail.com>
2021-03-20 15:49:53 +0530

    dynamic data

--- server.js
+++ server.js
@@ -1,33 +1,53 @@
 const express = require("express");
 const cors = require("cors");
+const User = require("./model/User");
 
 const app = express();
 
 const PORT = 8080 || process.env.PORT;
 
-app.use(cors())
+app.use(cors());
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-
 app.get("/", (_, res) => {
-    res.redirect(`/:no_username_home`);
+  res.redirect(`/:no_username_home`);
 });
 
-app.get("/:username", (req, res) => {
-  res.render("index", { userName: req.params.username });
+app.get("/:username", async (req, res) => {
+  const { username } = req.params;
+  const user = await User.findOne({ username });
+  if (username !== "no_username_home") {
+    if (user) {
+      res.render("index", {
+        userName: username,
+        backgroundColor: user.background,
+        bodyText: user.body_text
+      });
+    } else {
+      res.render("index", {
+        userName: username,
+        backgroundColor: undefined,
+        bodyText: undefined
+      });
+    }
+  } else {
+    res.render("index", {
+      userName: username,
+      backgroundColor: undefined,
+      bodyText: undefined
+    });
+  }
 });
 
-const server =
-  app.listen(PORT, () => {
-    console.log(`🚀 Server running at http://localhost:${PORT}`);
-  });
-;
+const server = app.listen(PORT, () => {
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
+});
+
 const io = require("socket.io")(server);
 
 io.on("connection", (_) => {
-    console.log('a user connected');
+  console.log("a user connected");
 });
-
 
 module.exports = io;
